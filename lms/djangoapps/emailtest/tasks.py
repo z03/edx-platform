4c6c6f71ecad0995b1f4219ad3693ce2744063d4
Jason Bau <jbau@stanford.edu>
2013-05-28 22:35:04 -0700

    sanity test for email

--- lms/djangoapps/emailtest/tasks.py
+++ lms/djangoapps/emailtest/tasks.py
@@ -1,6 +1,6 @@
 from djcelery import celery
-from django.core.mail import send_email
+from django.core.mail import send_mail
 
 @celery.task
 def email_task(subject, message, from_addr, to_list):
-    send_email(subject, message, from_addr, to_list)
+    send_mail(subject, message, from_addr, to_list)
