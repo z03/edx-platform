bdb0bfd4477e0b2f60a8451a27dee1d1dda1d8f0
Slater-Victoroff <slater.r.victoroff@gmail.com>
2013-06-13 19:06:45 -0400

    Added some intelligence to word highlighting to recognize similar words and added max_results argument to search
    Added mongo integration with elasticsearch requests module
    
    Coverted templates to use mako

--- common/djangoapps/search/views.py
+++ common/djangoapps/search/views.py
@@ -1,59 +1,92 @@
 from django.http import HttpResponse, Http404
-from django.template import Context, RequestContext
 from django.core.context_processors import csrf
-from django.template.loader import render_to_string
+from mitxmako.shortcuts import render_to_string
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 import requests
 import json
 import nltk.data
 import nltk.corpus as word_filter
 import enchant
-import os
+import string
 
 
 def search(request):
     context = {}
-    context.update((csrf(request)))
     results_string = ""
-    if request.POST:
+    if request.GET:
         results_string = find(request)
-    search_bar = render_to_string("search.html", Context(context))
+        context.update({"old_query": request.GET['s']})
+    search_bar = render_to_string("search.html", context)
     return HttpResponse(search_bar + results_string)
 
 
-def find(request, database="http://127.0.0.1:9200", index="transcript-index",
-         value_="transcript", field="searchable_text"):
-    query = request.POST['s']
+def find(request, database="http://127.0.0.1:9200",
+         value_="transcript", field="searchable_text", max_result=100):
+    query = request.GET.get("s", "")
+    page = request.GET.get("page", 1)
+    results_per_page = request.GET.get("results", 15)
+    ordering = request.GET.get("ordering", False)
+    index = request.GET.get("content", "transcript")+"-index"
     full_url = "/".join([database, index, value_, "_search?q="+field+":"])
-    results = json.loads(requests.get(full_url+query)._content)["hits"]["hits"]
-    uuids = [entry["_source"]["uuid"] for entry in results]
-    transcripts = [entry["_source"]["searchable_text"] for entry in results]
-    snippets = [snippet_generator(transcript, query) for transcript in transcripts]
+    context = {}
+
+    try:
+        results = json.loads(requests.get(full_url+query+"&size="+str(max_result))._content)["hits"]["hits"]
+        uuids = [entry["_source"]["uuid"] for entry in results]
+        transcripts = [entry["_source"]["searchable_text"] for entry in results]
+        snippets = [snippet_generator(transcript, query) for transcript in transcripts]
+        data = zip(uuids, snippets)
+        data = proper_page(page, data, results_per_page)
+    except KeyError:
+        data = ("No results found", "Please try again")
+    context.update({"data": data})
+
     correction = spell_check(query)
-    data = zip(uuids, snippets)
-    context = {"data": data, "search_correction": correction}
-    return render_to_string("search_templates/results.html", context, context_instance=RequestContext(request))
+    results_pages = Paginator(data, results_per_page)
+    context.update({"spelling_correction": correction})
+    return render_to_string("search_templates/results.html", context)
 
 
 def query_reduction(query, stopwords):
     return [word.lower() for word in query.split() if word not in stopwords]
 
 
+def proper_page(page, data, results_per_page=15):
+    pages = Paginator(data, results_per_page)
+    correct_page = ""
+    try:
+        correct_page = pages.page(page)
+    except PageNotAnInteger:
+        correct_page = pages.page(1)
+    except EmptyPage:
+        correct_page = pages.page(pages.num_pages)
+    return correct_page
+
+
+def match(words):
+    contained = lambda words: (words[0] in words[1]) or (words[1] in words[0])
+    near_size = lambda words: abs(len(words[0]) - len(words[1])) < (len(words[0])+len(words[1]))/6
+    return contained(words) and near_size(words)
+
+
 def match_highlighter(query, response, tag="b", css_class="highlight", highlight_stopwords=True):
     wrapping = ("<"+tag+" class="+css_class+">", "</"+tag+">")
+    punctuation_map = dict((ord(char), None) for char in string.punctuation)
+    depunctuation = lambda word: word.translate(punctuation_map)
     wrap = lambda text: wrapping[0] + text + wrapping[1]
     stop_words = word_filter.stopwords.words("english") * (not highlight_stopwords)
     query_set = set(query_reduction(query, stop_words))
     bold_response = ""
     for word in response.split():
-        if any(query_word in word.lower() for query_word in query_set):
+        if any(match((query_word, depunctuation(word.lower()))) for query_word in query_set):
             bold_response += wrap(word) + " "
         else:
             bold_response += word + " "
     return bold_response
 
 
-def snippet_generator(transcript, query, soft_max=100, word_margin=50, bold=True):
+def snippet_generator(transcript, query, soft_max=50, word_margin=30, bold=True):
     punkt = nltk.data.load('tokenizers/punkt/english.pickle')
     stop_words = word_filter.stopwords.words("english")
     sentences = punkt.tokenize(transcript)
@@ -68,6 +101,14 @@ def snippet_generator(transcript, query, soft_max=100, word_margin=50, bold=True
                 response += sentence
         else:
             if (len(response.split()) + len(sentence.split()) < soft_max):
+                response += " " + sentence
+            else:
+                response += " " + " ".join(sentence.split()[:word_margin])
+                break
+    # If this is a phonetic match, there might not be a direct text match
+    if tripped is False:
+        for sentence in sentences:
+            if (len(response.split())+len(sentence.split())) < soft_max:
                 response += " " + sentence
             else:
                 response += " " + " ".join(sentence.split()[:word_margin])
