247d089068a24c522b5ad3b2e4322586470362b1
Chris Dodge <cdodge@edx.org>
2013-08-18 21:10:09 -0400

    tighen up the code and put the stats dump after a period of time, 1 hour default

--- common/djangoapps/mongo_perf_tracker/middleware.py
+++ common/djangoapps/mongo_perf_tracker/middleware.py
@@ -2,6 +2,7 @@
 Provide a means to aggregate and store MongoDB related performance in our product
 """
 import threading
+import time
 import logging
 from xmodule.contentstore.content import XASSET_LOCATION_TAG
 from django.conf import settings
@@ -12,12 +13,12 @@ _mongo_perf_tracker_threadlocal.data = {}
 # counter to keep track of how many requests we've processed. This is used to
 # keer track of when we need to flush out internal buffers out to some persistence
 # layer
-trackable_requests_processed = 0
+last_dump_time = time.time()
 
 # global object which retains a mapping of URL to DB performance information
 db_stats_per_url = {}
 
-log = logging.getLogger("lms.mongo_perf_tracker")
+log = logging.getLogger("common.mongo_perf_tracker")
 
 
 class MongoPerfTracker(object):
@@ -25,7 +26,8 @@ class MongoPerfTracker(object):
     Implements Django middleware object which manages the lifetime of
     the performance tracker for our MongoDB store provider
     """
-    def get_perf_tracker_data(self):
+    @property
+    def perf_tracker_data(self):
         """
         Returns the dictionary that is in the threadlocal
         """
@@ -41,19 +43,16 @@ class MongoPerfTracker(object):
         """
         Increases a counter by one
         """
-        if key in _mongo_perf_tracker_threadlocal.data:
-            _mongo_perf_tracker_threadlocal.data[key] = _mongo_perf_tracker_threadlocal.data[key] + 1
-        else:
-            _mongo_perf_tracker_threadlocal.data[key] = 1
+        self.add_to_float_perf_tracker_counter(key, 1.0)
 
     def add_to_float_perf_tracker_counter(self, key, increment_float):
         """
         Adds a number to a tracking element which is a float
         """
-        if key in _mongo_perf_tracker_threadlocal.data:
-            _mongo_perf_tracker_threadlocal.data[key] = _mongo_perf_tracker_threadlocal.data[key] + increment_float
-        else:
-            _mongo_perf_tracker_threadlocal.data[key] = increment_float
+        counter = self.perf_tracker_data[key] if key in self.perf_tracker_data else 0.0
+        counter = counter + increment_float
+
+        self.set_perf_tracker_data_entry(key, counter)
 
     def _is_trackable_path(self, request):
         """
@@ -91,37 +90,40 @@ class MongoPerfTracker(object):
         """
         Django middleware entry point that is called on every response sent back to client
         """
-        global trackable_requests_processed, db_stats_per_url
-        if self._is_trackable_path(request):
-            # copy over any stats gathered in this request and put in the global dictionary
-            # to get written out on a periodic basis
+        global trackable_requests_processed, db_stats_per_url, last_dump_time
 
-            set_entry = True
+        try:
+            if self._is_trackable_path(request):
+                # copy over any stats gathered in this request and put in the global dictionary
+                # to get written out on a periodic basis
 
-            # take what the overwrite key should be from settings, if defined
-            overwrite_key = getattr(settings, 'MONGO_PERF_TRACKER_OVERWITE_KEY', None)
+                set_entry = True
 
-            # first see if we have an entry for this path, if so see if an overwrite key
-            # has been specified so that we can use that value to compare to what exists
-            # this can be used to implement a high-water mark
-            if request.path in db_stats_per_url and overwrite_key:
-                existing_level = db_stats_per_url[request.path].get(overwrite_key, 0)
-                new_level = _mongo_perf_tracker_threadlocal.data.get(overwrite_key, 0)
-                set_entry = existing_level < new_level
+                # take what the overwrite key should be from settings, if defined
+                overwrite_key = getattr(settings, 'MONGO_PERF_TRACKER_OVERWITE_KEY', None)
 
-            if set_entry and len(_mongo_perf_tracker_threadlocal.data.keys()) > 0:
-                db_stats_per_url[request.path] = _mongo_perf_tracker_threadlocal.data
+                # first see if we have an entry for this path, if so see if an overwrite key
+                # has been specified so that we can use that value to compare to what exists
+                # this can be used to implement a high-water mark
+                if request.path in db_stats_per_url and overwrite_key:
+                    existing_level = db_stats_per_url[request.path].get(overwrite_key, 0)
+                    new_level = self.perf_tracker_data.get(overwrite_key, 0)
+                    set_entry = existing_level < new_level
 
-            trackable_requests_processed = trackable_requests_processed + 1
+                if set_entry and len(self.perf_tracker_data.keys()) > 0:
+                    db_stats_per_url[request.path] = self.perf_tracker_data
 
-            dump_limit = getattr(settings, 'MONGO_PERF_DUMP_AFTER_N_REQUESTS', 1)
+                # dump stats after a certain period of time, default 1 hr
+                dump_time_delta = getattr(settings, 'MONGO_PERF_DUMP_AFTER_N_MINUTES', 60.0) * 60.0
 
-            if trackable_requests_processed > dump_limit:
-                # Note, we use WARN level so they don't get filtered out in the logs
-                log.warning('mongo_db_stats dump = {0}'.
-                            format(db_stats_per_url))
-                trackable_requests_processed = 0
-                db_stats_per_url = {}
+                if time.time() - last_dump_time > dump_time_delta and len(db_stats_per_url.keys()) > 0:
+                    # Note, we use info level so they don't get filtered out in the logs
+                    log.info('mongo_db_stats dump = {0}'.format(db_stats_per_url))
+                    last_dump_time = time.time()
+                    db_stats_per_url = {}
+        except:
+            # This is just optional perf metering, so trap all unhandled exceptions and continue
+            pass
 
         self.clear_perf_tracker_data()
         return response
